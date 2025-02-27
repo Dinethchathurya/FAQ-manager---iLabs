839902546d8a732ad7ee7863b7319e03321bac10
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2025-02-28 00:54:07 +0530

    models are created

---
+++ faqmanager/src/main/java/com/faqmanager/faqmanager/model/Questions.java
@@ -0,0 +1,30 @@
+package com.faqmanager.faqmanager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "questions")
+public class Questions {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String question;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category categoryId;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private String action;
+}
