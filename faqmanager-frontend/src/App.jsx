a3ea27688e8a06113a724b559867882bedd73d9d
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2025-02-28 23:47:58 +0530

    navBar and search section

--- faqmanager-frontend/src/App.jsx
+++ faqmanager-frontend/src/App.jsx
@@ -1,57 +1,13 @@
 import { useState } from "react";
+import Header from "./components/Header";
+import SearchSectionComponent from "./components/SearchSectionComponent";
 
 function App() {
   return (
     <>
+      <Header />
+      <SearchSectionComponent/>
       <div className="container">
-      <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-          <a
-            href="/"
-            class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none"
-          >
-            <svg class="bi me-2" width="40" height="32">
-              <use xlink:href="#bootstrap"></use>
-            </svg>
-            <span class="fs-4">FAQ manager - iLabs</span>
-          </a>
-
-          <ul class="nav nav-pills">
-            <li class="nav-item">
-              <a href="#"  aria-current="page">
-                <button type="button" class="btn  shadow-custom">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-plus-circle-fill me-2"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"></path>
-                  </svg>
-                  Add New Question
-                </button>
-              </a>
-            </li>
-          </ul>
-        </header>
-      </div>
-      <div className="container">
-        <div>
-          <div className="d-flex justify-content-between mb-3">
-            <input
-              type="text"
-              className="form-control w-50"
-              placeholder="Search..."
-            />
-            <button className="btn btn-primary">Search</button>
-          </div>
-        </div>
-
-        <div className="d-flex justify-content-end mb-3">
-          <button className="btn btn-primary">+ Add New Question</button>
-        </div>
-
         <table className="table table-bordered">
           <thead className="table-light">
             <tr>
---
+++ faqmanager-frontend/src/components/SearchSectionComponent.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function SearchSectionComponent() {
+  return (
+    <>
+      <div className="container">
+        <div className="search-background mb-3 p-3">
+          <div className="d-flex">
+            <input
+              type="text"
+              className="form-control w-75 me-2 light-blue"
+              placeholder="Search..."
+            />
+            <button className="btn dark-blue w-25">Search</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default SearchSectionComponent;
