b9ff00fc71ef9b1ac5e897e605a0ac42648c687f
Dineth Chathurya <130559325+Dinethchathurya@users.noreply.github.com>
2025-03-01 23:47:54 +0530

    add modal for add question

--- faqmanager-frontend/src/components/Header.jsx
+++ faqmanager-frontend/src/components/Header.jsx
@@ -1,3 +1,5 @@
+import AddQuestionModal from "./AddQuestionModal";
+
 function Header() {
   return (
     <>
@@ -15,27 +17,31 @@ function Header() {
 
           <ul className="nav nav-pills">
             <li className="nav-item">
-              <a href="#" aria-current="page">
-                <button
-                  type="button"
-                  className="btn  shadow-custom add-question-button"
+              <button
+                type="button"
+                className="btn  shadow-custom add-question-button"
+                data-bs-toggle="modal"
+                data-bs-target="#addQuestionModal"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="#4350AF"
+                  className="bi bi-plus-circle-fill me-2 "
+                  viewBox="0 0 16 16"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="#4350AF"
-                    className="bi bi-plus-circle-fill me-2 "
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"></path>
-                  </svg>
-                  Add New Question
-                </button>
-              </a>
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z"></path>
+                </svg>
+                Add New Question
+              </button>
+            
             </li>
           </ul>
         </header>
+
+        <AddQuestionModal />
+
       </div>
     </>
   );
